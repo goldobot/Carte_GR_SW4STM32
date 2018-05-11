839e7c5a529c106bf44b4f041ab12d341cd7be09
Thomas Liennard <thomas.liennard@gmail.com>
2018-05-11 10:04:12 +0200

    add limitation of acceleration and speed, yaw_rate low pass filter to robot simulator.

--- goldobot/src/robot_simulator.cpp
+++ goldobot/src/robot_simulator.cpp
@@ -25,8 +25,27 @@ RobotSimulator::RobotSimulator() :
 void RobotSimulator::do_step()
 {
 	// Simple formula for speed and yaw, with perfect motor controllers.
-	m_speed = 0.5 * (m_left_pwm + m_right_pwm) * m_config.speed_coeff;
-	m_yaw_rate = (m_right_pwm - m_left_pwm) * m_config.speed_coeff / m_config.wheels_spacing;
+	float target_speed = 0.5 * (m_left_pwm + m_right_pwm) * m_config.speed_coeff;
+	float target_yaw_rate = (m_right_pwm - m_left_pwm) * m_config.speed_coeff / m_config.wheels_spacing;
+
+	// Low pass filter
+	target_speed = target_speed * 0.5 + 0.5 * m_speed;
+	target_yaw_rate = target_yaw_rate * 0.5 + 0.5 * m_yaw_rate;
+
+	//Limit acceleration
+	float acceleration_limit = 2;
+	if(target_speed > m_speed + 1e-3 * acceleration_limit)
+	{
+		m_speed = m_speed + 1e-3 * acceleration_limit;
+	} else if(target_speed < m_speed - 1e-3 * acceleration_limit)
+	{
+		m_speed = m_speed - 1e-3 * acceleration_limit;
+	} else
+	{
+		m_speed = target_speed;
+	}
+
+	m_yaw_rate = target_yaw_rate;
 
 	// Update robot pose and wheels distances
 	double d_yaw = 1e-3 * m_yaw_rate;
