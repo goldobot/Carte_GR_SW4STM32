c1e95d3e028206d4f21e3ed97e60f3cb76370c41
Thomas Liennard <thomas.liennard@gmail.com>
2018-06-10 15:46:21 +0200

    debug main sequencer, add second speed setting.

--- goldobot/src/tasks/main.cpp
+++ goldobot/src/tasks/main.cpp
@@ -24,6 +24,7 @@ MainTask::MainTask():
 {
 	m_dbg_message_queue_mutex = xSemaphoreCreateMutex();
 	m_speed_settings[0] = {0.5,1,1,2,1,1,0.2};
+	m_speed_settings[1] = {0.25,0.5,0.5,1,0.5,0.5,0.2};
 }
 
 const char* MainTask::name() const
@@ -106,6 +107,13 @@ void MainTask::matchSelectNextObjective()
 	m_trajectory_planner.compute_trajectory(m_current_objective);
 }
 
+void MainTask::execute_sequence(int seq_id)
+{
+	m_current_sequence_id = seq_id;
+	m_current_command_id = m_sequences[m_current_sequence_id].begin_idx;
+	m_sequence_active = true;
+	m_wait_current_cmd = false;
+}
 void MainTask::sequence_step()
 {
 	if(!m_sequence_active)
@@ -120,110 +128,13 @@ void MainTask::sequence_step()
 	//exec
 	if(!m_wait_current_cmd)
 	{
-		float delta_yaw=0;
-		switch(cmd.opcode)
-		{
-		case OpCode::Delay:
-			m_wait_current_cmd = true;
-			m_delay_finished_ts = xTaskGetTickCount() + cmd.delay_ms;
-			break;
-		case OpCode::SetPose:
-			propulsion.reset_pose(m_waypoints[cmd.set_pose.pose_id].x, m_waypoints[cmd.set_pose.pose_id].y, cmd.set_pose.angle_deg * M_PI / 180);
-			break;
-
-		case OpCode::Rotation:
-			delta_yaw = cmd.rotation.angle * M_PI / 180;
-			propulsion.executeRotation(delta_yaw,1,1,1);
-			m_wait_current_cmd = cmd.blocking;
-			break;
-		case OpCode::Reposition:
-			{
-				auto dir = PropulsionController::Direction::Forward;
-				float angle_rad = cmd.reposition.angle * M_PI/180;
-				Vector2D normal = {cosf(angle_rad), sinf(angle_rad)};
-
-				// Check angle of current robot position compared to border
-				auto pose = Robot::instance().odometry().pose();
-				float ux = cos(pose.yaw);
-				float uy = sinf(pose.yaw);
-				float dist_m = cmd.reposition.distance*1e-3;
-
-				if(ux * normal.x + uy * normal.y >= 0)
-				{
-					dir = PropulsionController::Direction::Forward;
-					dist_m -= Robot::instance().robotConfig().front_length;
-				} else
-				{
-					dir = PropulsionController::Direction::Backward;
-					dist_m -= Robot::instance().robotConfig().back_length;
-				}
-
-				propulsion.executeRepositioning(dir, 0.2,normal,dist_m);
-				m_wait_current_cmd = cmd.blocking;
-			}
-			break;
-		case OpCode::Trajectory:
-		{
-			Vector2D points[16];
-			for(unsigned i=0;i < cmd.trajectory.num_points;i++)
-			{
-				int pt_id = m_trajectory_points[cmd.trajectory.begin_idx + i];
-				points[i] = m_waypoints[pt_id];
-			}
-			auto& ss = m_speed_settings[cmd.trajectory.speed_settings];
-			propulsion.executeTrajectory(points,cmd.trajectory.num_points,ss.speed, ss.acceleration, ss.decceleration);
-			m_wait_current_cmd = cmd.blocking;
-		}
-		break;
-		case OpCode::ArmsGoToPosition:
-			arms.go_to_position(cmd.arms.arm_id, cmd.arms.seq_or_pos_id,0);
-			m_wait_current_cmd = cmd.blocking;
-			break;
-		case OpCode::ArmsExecuteSequence:
-			arms.execute_sequence(cmd.arms.arm_id, cmd.arms.seq_or_pos_id);
-			m_wait_current_cmd = cmd.blocking;
-			break;
-		default:
-			break;
-		}
+		m_wait_current_cmd = _execute_command(cmd) && cmd.blocking;
 	}
+
 	// waiting
 	if(m_wait_current_cmd)
 	{
-		switch(cmd.opcode)
-		{
-		case OpCode::Delay:
-			if(m_delay_finished_ts >= xTaskGetTickCount())
-			{
-				m_wait_current_cmd = false;
-			}
-
-			break;
-		case OpCode::Rotation:
-			if(propulsion.state() == PropulsionController::State::Stopped)
-			{
-				m_wait_current_cmd = false;
-			}
-			break;
-		case OpCode::Reposition:
-			if(propulsion.state() == PropulsionController::State::Stopped)
-			{
-				m_wait_current_cmd = false;
-			}
-			break;
-		case OpCode::Trajectory:
-			if(propulsion.state() == PropulsionController::State::Stopped)
-			{
-				m_wait_current_cmd = false;
-			}
-			break;
-		case OpCode::ArmsGoToPosition:
-			m_wait_current_cmd = false;
-			break;
-		case OpCode::ArmsExecuteSequence:
-			m_wait_current_cmd = arms.m_arms_moving[cmd.arms.arm_id];
-			break;
-		}
+		m_wait_current_cmd = !_current_command_finished(cmd);
 	}
 
 	if(!m_wait_current_cmd)
@@ -235,11 +146,120 @@ void MainTask::sequence_step()
 		}
 	}
 }
+bool MainTask::_execute_command(const Command& cmd)
+{
+	float delta_yaw=0;
+	auto& arms = Robot::instance().arms();
+	auto& propulsion = Robot::instance().propulsion();
+
+	switch(cmd.opcode)
+	{
+	case OpCode::Delay:
+		m_delay_finished_ts = xTaskGetTickCount() + cmd.delay_ms;
+		return true;
+	case OpCode::SetPose:
+		propulsion.reset_pose(m_waypoints[cmd.set_pose.pose_id].x, m_waypoints[cmd.set_pose.pose_id].y, cmd.set_pose.angle_deg * M_PI / 180);
+		return false;
+	case OpCode::Rotation:
+	{
+		delta_yaw = cmd.rotation.angle * M_PI / 180;
+		auto& ss = m_speed_settings[cmd.trajectory.speed_settings];
+		propulsion.executeRotation(delta_yaw,ss.yaw_rate,ss.yaw_acceleration, ss.yaw_decceleration);
+		return true;
+	}
+	case OpCode::Reposition:
+		{
+			auto dir = PropulsionController::Direction::Forward;
+			float angle_rad = cmd.reposition.angle * M_PI/180;
+			Vector2D normal = {cosf(angle_rad), sinf(angle_rad)};
+
+			// Check angle of current robot position compared to border
+			auto pose = Robot::instance().odometry().pose();
+			float ux = cos(pose.yaw);
+			float uy = sinf(pose.yaw);
+			float dist_m = cmd.reposition.distance*1e-3;
+
+			if(ux * normal.x + uy * normal.y >= 0)
+			{
+				dir = PropulsionController::Direction::Forward;
+				dist_m -= Robot::instance().robotConfig().front_length;
+			} else
+			{
+				dir = PropulsionController::Direction::Backward;
+				dist_m -= Robot::instance().robotConfig().back_length;
+			}
+
+			propulsion.executeRepositioning(dir, 0.2,normal,dist_m);
+			return true;
+		}
+	case OpCode::Trajectory:
+	{
+		Vector2D points[16];
+		for(unsigned i=0;i < cmd.trajectory.num_points;i++)
+		{
+			int pt_id = m_trajectory_points[cmd.trajectory.begin_idx + i];
+			points[i] = m_waypoints[pt_id];
+		}
+		auto& ss = m_speed_settings[cmd.trajectory.speed_settings];
+		propulsion.executeTrajectory(points,cmd.trajectory.num_points,ss.speed, ss.acceleration, ss.decceleration);
+		return true;
+	}
+	case OpCode::ArmsGoToPosition:
+		arms.go_to_position(cmd.arms.arm_id, cmd.arms.seq_or_pos_id,0);
+		return true;
+		break;
+	case OpCode::ArmsExecuteSequence:
+		arms.execute_sequence(cmd.arms.arm_id, cmd.arms.seq_or_pos_id);
+		return true;
+	default:
+		return false;
+	}
+}
+bool MainTask::_current_command_finished(const Command& cmd)
+{
+	auto& arms = Robot::instance().arms();
+	auto& propulsion = Robot::instance().propulsion();
+
+	switch(cmd.opcode)
+	{
+	case OpCode::Delay:
+		if(xTaskGetTickCount() >= m_delay_finished_ts)
+		{
+			return true;
+		}
+		break;
+	case OpCode::Rotation:
+		if(propulsion.state() == PropulsionController::State::Stopped)
+		{
+			return true;
+		}
+		break;
+	case OpCode::Reposition:
+		if(propulsion.state() == PropulsionController::State::Stopped)
+		{
+			return true;
+		}
+		break;
+	case OpCode::Trajectory:
+		if(propulsion.state() == PropulsionController::State::Stopped)
+		{
+			return true;
+		}
+		break;
+	case OpCode::ArmsGoToPosition:
+		return true;
+		break;
+	case OpCode::ArmsExecuteSequence:
+		return !arms.m_arms_moving[cmd.arms.arm_id];
+		break;
+	default:
+		break;
+	}
+	return false;
+}
 
 void MainTask::taskFunction()
 {
-	// Dirty. init points
-
 	m_trajectory_planner.add_point(0.24, -1.28);// 1: green starting position
 	m_trajectory_planner.add_point(0.24, -0.65);
 	m_trajectory_planner.add_point(0.3, -0.65);
@@ -259,6 +279,7 @@ void MainTask::taskFunction()
 
 	while(1)
 	{
+
 		uint32_t clock = xTaskGetTickCount();
 		process_messages();
 		sequence_step();
@@ -268,34 +289,61 @@ void MainTask::taskFunction()
 		case State::Idle:
 			if(!Hal::get_gpio(1))
 			{
-				m_match_state = State::WaitForStartOfMatch;
-			}
+				if( Hal::get_gpio(4))
+				{
+					m_side = Side::Green;
+				} else
+				{
+					m_side = Side::Orange;
+				}
 
+				m_match_state = State::WaitForStartOfMatch;
+				Hal::set_motors_enable(true);
+				Robot::instance().propulsion().enable();
+				switch(m_side)
+				{
+				case Side::Green:
+					execute_sequence(0);
+					break;
+				case Side::Orange:
+					execute_sequence(1);
+					break;
+				default:
+					break;
+				}
+			}
 			break;
 		case State::WaitForStartOfMatch:
 			if(Hal::get_gpio(1))
 			{
-				matchBegin();
+				switch(m_side)
+				{
+				case Side::Green:
+					execute_sequence(2);
+					m_match_state = State::Match;
+					break;
+				case Side::Orange:
+					execute_sequence(3);
+					m_match_state = State::Match;
+					break;
+				default:
+					break;
+				}
 			}
 			break;
 
 		case State::Match:
 			{
-				if(remainingMatchTime() < 70)
+				if(remainingMatchTime() == 0)
 				{
 					Hal::set_gpio(0, false);
 					m_match_state = State::PostMatch;
 					comm.send_message(CommMessageType::EndOfMatch,(char*)&clock,sizeof(clock));
 					Hal::set_motors_enable(false);
-				} else
-				{
-					matchStep();
 				}
 			}
 			break;
 		case State::PostMatch:
-
-			//m_match_state = State::Idle;
 			break;
 		}
 		vTaskDelay(1);
@@ -640,7 +688,7 @@ void MainTask::on_msg_dbg_arms_set_pose()
 	unsigned char buff[12];
 	pop_message(buff,12);
 
-	auto& arm_descr = arms.m_arms_descr[buff[0]];
+	auto& arm_descr = arms.m_arms_descrs[buff[0]];
 
 	uint16_t* ptr = arms.m_arms_positions+arm_descr.positions_idx_begin + buff[1]*arm_descr.num_servos;
 	memcpy(ptr, buff+2, arm_descr.num_servos * 2);
