a6c1e3ece1b3b8aca8ebe4f74f22fd0575de56d4
Thomas Liennard <thomas.liennard@gmail.com>
2021-01-03 17:50:01 +0100

    TASK: fix read buffer overflow in odrive parser and cleanup.

--- goldobot/include/goldobot/odrive/odrive_stream_parser.hpp
+++ goldobot/include/goldobot/odrive/odrive_stream_parser.hpp
@@ -5,18 +5,39 @@ namespace goldobot {
 
 class ODriveStreamParser {
  public:
+  struct Statistics {
+    uint32_t bytes_received{0};
+    uint16_t messages_received{0};
+    uint16_t rx_errors{0};
+  };
+
+ public:
   ODriveStreamParser(uint8_t* buffer, size_t buffer_size);
   size_t pushData(unsigned char* buffer, size_t size);
 
   bool packetReady() const;
   size_t packetSize() const;
-  void popPacket(uint8_t* buffer, size_t size);
+  size_t popPacket(uint8_t* buffer, size_t size);
+
+  // Return send statistics for the last period and reset them to zero.
+  Statistics statistics();
 
  private:
+  enum class State : uint8_t {
+    SearchMagic,
+    ReadHeader,
+    CheckHeaderCrc,
+    ReadPayload,
+    PacketReady,
+  };
+
+  static constexpr size_t c_max_packet_size = 128;
+
   uint8_t m_packet_body[130];
-  uint8_t m_state{0};
-  uint8_t m_packet_length;
-  uint8_t m_bytes_read;
+  State m_state{State::SearchMagic};
+  uint8_t m_packet_length{0};
+  uint8_t m_bytes_read{0};
+  Statistics m_statistics;
 };
 
 }  // namespace goldobot
