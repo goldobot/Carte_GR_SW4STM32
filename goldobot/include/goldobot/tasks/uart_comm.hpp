6304d3c408957b2f8aa5d8dd366e83644e68957b
Thomas Liennard <thomas.liennard@thomas-liennard.fr>
2021-06-17 15:45:18 +0200

    move heartbeat into uart_comm task, add ftdi uart (disabled for now)

--- goldobot/include/goldobot/tasks/uart_comm.hpp
+++ goldobot/include/goldobot/tasks/uart_comm.hpp
@@ -9,12 +9,17 @@
 namespace goldobot {
 class UARTCommTask : public Task {
  public:
-  struct Statistics
-  {
-	  uint32_t max_cycles{0};
-	  MessageQueue::Statistics out_queue;
-	  MessageQueue::Statistics out_prio_queue;
+  struct Statistics {
+    uint32_t max_cycles{0};
+    CommSerializer::Statistics serializer;
+    CommDeserializer::Statistics deserializer;
+    CommSerializer::Statistics serializer_ftdi;
+    CommDeserializer::Statistics deserializer_fdti;
+    MessageQueue::Statistics out_queue;
+    MessageQueue::Statistics out_prio_queue;
+    MessageQueue::Statistics out_ftdi_queue;
   };
+
  public:
   UARTCommTask();
   const char* name() const override;
@@ -27,18 +32,34 @@ class UARTCommTask : public Task {
   bool send_message(CommMessageType msg_type, const char* buffer, uint16_t size);
 
   void sendStatistics();
+  void sendHeartbeat(uint32_t timestamp);
 
-  uint32_t m_last_timestamp;
+  std::atomic<bool> m_ftdi_enable{false};
 
-  static unsigned char s_scratch_buffer[1024];
-  static unsigned char s_serialize_buffer[1024];
-  static unsigned char s_deserialize_buffer[1024];
+  uint32_t m_next_heartbeat_timestamp{0};
+  uint32_t m_next_statistics_timestamp{0};
+
+  static unsigned char s_scratch_buffer[512];
+
+  static unsigned char s_serialize_buffer[512];
+  static unsigned char s_deserialize_buffer[512];
+
+  static unsigned char s_serialize_ftdi_buffer[512];
+  static unsigned char s_deserialize_ftdi_buffer[512];
+
   static unsigned char s_out_buffer[512];
-  static unsigned char s_out_prio_buffer[512];
+  static unsigned char s_out_prio_buffer[1024];
+  static unsigned char s_out_ftdi_buffer[512];
+
   CommSerializer m_serializer;
   CommDeserializer m_deserializer;
+
+  CommSerializer m_serializer_ftdi;
+  CommDeserializer m_deserializer_ftdi;
+
   MessageQueue m_out_queue;
   MessageQueue m_out_prio_queue;
+  MessageQueue m_out_ftdi_queue;
 
   Statistics m_statistics;
 };
