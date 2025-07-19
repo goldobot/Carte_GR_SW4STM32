22b10a69d1c754a127663ea9741a6036d877b88e
Goldorak <echelon@free.fr>
2025-07-19 23:31:27 +0200

    * WIP:   - analyse du probleme de l'arret d'urgence finie => la cause primordiale etait la non atteinte de la valeur limite "m_max_parameter" (a cause de "l'echantillonage trop grossier"??)   - tentative de solution un peu plus "clean".. (avec implementation d'un mode special "m_emergency")

--- libs/goldo_propulsion/include/goldobot/propulsion/speed_controller.hpp
+++ libs/goldo_propulsion/include/goldobot/propulsion/speed_controller.hpp
@@ -62,5 +62,9 @@ class SpeedController {
   float m_c3[8];
   float m_t[8];
   unsigned m_num_points{0};
+
+  // emergency stop management
+  bool m_emergency{false};
+  float m_emergency_stop_time_limit{0};
 };
 }  // namespace goldobot
